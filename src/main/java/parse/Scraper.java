150aba5336b1803aece95030f77d82a8d3c30262
Newton Ni <nwtnni@gmail.com>
2017-08-17 17:16:16 -0400

    Initial commit

---
+++ src/main/java/parse/Scraper.java
@@ -0,0 +1,33 @@
+package parse;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Scraper {
+	
+	private ArrayList<Thread> threads;
+	
+	public void parse(File f) throws IOException {
+	
+		Document doc = Jsoup.parse(f, null);
+
+		threads = new ArrayList<>();
+		doc.select(".thread").forEach(thread -> threads.add(new Thread(thread)));
+		System.out.println(doc.select("h1").first().text());
+		
+	}
+	
+	public static void main(String[] args) {
+		File f = new File(args[0]);
+		Scraper s = new Scraper();
+		try {
+			s.parse(f);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/parse/Thread.java
@@ -0,0 +1,10 @@
+package parse;
+
+import org.jsoup.nodes.Element;
+
+public class Thread {
+	
+	public Thread(Element e) {
+		
+	}
+}
